3395bf5b09cef1329444665a8696381ca458889f
Fim-84 <marcus.stenberg@gmail.com>
2023-08-07 15:51:59 +0200

    small changes

--- x/ugdmint/types/minter_test.go
+++ x/ugdmint/types/minter_test.go
@@ -269,7 +269,7 @@ func TestAddressConvertion(t *testing.T) {
 	testCtx := testutil.DefaultContextWithDB(t, key, sdk.NewTransientStoreKey("transient_test"))
 	ctx := testCtx.Ctx.WithBlockHeader(types.Header{ChainID: "unigrid", Time: time.Now()})
 	fmt.Println(ctx.BlockHeader().ChainID)
-	fmt.Println(ctx.BlockHeader())
+	//fmt.Println(ctx.BlockHeader())
 
 	ctrl := gomock.NewController(t)
 	acctKeeper := govtestutil.NewMockAccountKeeper(ctrl)
