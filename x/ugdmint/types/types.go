a95c7bed6280cba56cbf6e9ea4b2b158e84c014d
dekm <gevan73@gmail.com>
2024-03-09 15:24:57 +0100

    remove duplicate decleration of MintRecord in types.go as it will be generated by the proto file

--- x/ugdmint/types/types.go
+++ x/ugdmint/types/types.go
@@ -1,12 +1,1 @@
 package types
-
-import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
-)
-
-// MintRecord defines the structure for minting records
-type MintRecord struct {
-	BlockHeight int64     `json:"block_height"`
-	Account     string    `json:"account"`
-	Amount      sdk.Coins `json:"amount"`
-}
